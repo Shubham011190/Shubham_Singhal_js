314281d8e9af591ac7eeba34e7d05aa9cebcc66d
Shubham011190 <shubham.011190@gmail.com>
2020-12-01 08:44:13 +0530

    Sidebar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Topbar from './components/topbar/topbar';
+import Sidemenu from './components/sidemenu/sidemenu';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <Sidemenu />
     </div>
   );
 }
---
+++ src/components/sidemenu/sidemenu.js
@@ -0,0 +1,21 @@
+import "./sidemenu.css";
+const sidemenu =() =>{
+    return(
+        <div className="containerSidemenu">
+            <ul id ="ULSidemenu">
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+        </div>
+    )
+}
+
+export default sidemenu;
